fd2f1f01a7f24cbaa15a9a94c000a71e85caaed3
Eduardo-Valente <ev4lente@gmail.com>
2021-05-05 14:05:32 -0300

    Calculado o total de visualizações considerando o anúncio original mais os compartilhamentos

--- src/aplicacao/calculadora.java
+++ src/aplicacao/calculadora.java
@@ -4,37 +4,44 @@ import java.util.Scanner;
 
 public class calculadora {
 
-	public static final int TRINTA_VISUALIZACOES_ANUNCIO_NAO_COMPARTILHADO = 30;
+	public static final int VISUALIZACOES_ANUNCIO_NAO_COMPARTILHADO_A_CADA_UM_REAL = 30;
 	
-	public static final int CEM_VISUALIZACOES = 100;
-	public static final int DOZE_CLIQUES_ANUNCIO = 12;
+	public static final int VISUALIZACOES = 100;
+	public static final int CLIQUES_ANUNCIO = 12;
 	
-	public static final int VINTE_ACESSOS = 20;
-	public static final int TRES_COMPARTILHAMENTOS_ANUNCIO = 3;
+	public static final int ACESSOS_ANUNCIO = 20;
+	public static final int COMPARTILHAMENTOS_ANUNCIO = 3;
+	public static final int NUMERO_POS_SEQUENCIA = 5;
+	
+	public static final int VISUALIZACOES_ANUNCIO_COMPARTILHADO = 40;
 	
 	public static void main(String[] args) {
 		
 	Scanner sc = new Scanner(System.in);
 	
 	int dinheiroInvestido = 0;
-	int qtdVisualizacoesAnuncio = 0;
+	int qtdVisualizacoesAnuncioNaoCompartilhado = 0;
 	int qtdCliquesAnuncio = 0;
 	int qtdCompartilhamentoRedeSocial = 0;
+	int qtdVisualizacoesAnuncioCompartilhado = 0;
+	int totalVisualizacoes = 0;
+	
 	
 	System.out.print("Por favor informe a quantia inteira de dinheiro investida: ");
 	dinheiroInvestido = sc.nextInt();
 	
-	qtdVisualizacoesAnuncio = calculaQuantidadeVisualizacoesDoAnuncioNaoCompartilhado(dinheiroInvestido); 
+	qtdVisualizacoesAnuncioNaoCompartilhado = calculaQuantidadeVisualizacoesDoAnuncioNaoCompartilhado(dinheiroInvestido); 
 	
-	System.out.printf("O anúncio não compartilhado foi visto %d vezes.\n", qtdVisualizacoesAnuncio);
+	qtdCliquesAnuncio = calculaQuantidadeCliquesNoAnuncio(qtdVisualizacoesAnuncioNaoCompartilhado);
 	
-	qtdCliquesAnuncio = calculaQuantidadeCliquesNoAnuncio(qtdVisualizacoesAnuncio);
+	qtdCompartilhamentoRedeSocial = calculaQuantidadeCompartilhamentosEmSequenciaNaRedeSocial(
+									calculaQuantidadeCompartilhamentosNaRedeSocial(qtdCliquesAnuncio) );
 	
-	System.out.printf("O anúncio foi clicado %d vezes.\n", qtdCliquesAnuncio);
+	qtdVisualizacoesAnuncioCompartilhado = calculaQuantidadeVisualizacoesDoAnuncioCompartilhado(qtdCompartilhamentoRedeSocial);
 	
-	qtdCompartilhamentoRedeSocial = calculaQuantidadeCompartilhamentoRedeSocial(qtdCliquesAnuncio);
+	totalVisualizacoes = qtdVisualizacoesAnuncioNaoCompartilhado + qtdVisualizacoesAnuncioCompartilhado;
 	
-	System.out.printf("O anúncio foi compartilhado %d vezes.\n", qtdCompartilhamentoRedeSocial);
+	System.out.printf("%d pessoas visualizaram o anúncio no total.\n", totalVisualizacoes);
 	
 	sc.close();
 	
@@ -48,35 +55,65 @@ public class calculadora {
 			System.exit(1);
 		}
 		    
-		return TRINTA_VISUALIZACOES_ANUNCIO_NAO_COMPARTILHADO * dinheiroInvestido;
+		return VISUALIZACOES_ANUNCIO_NAO_COMPARTILHADO_A_CADA_UM_REAL * dinheiroInvestido;
 		    
 	}
 	
-	public static int calculaQuantidadeCliquesNoAnuncio(int qtdVisualizacoesAnuncio)
+	public static int calculaQuantidadeCliquesNoAnuncio(int qtdVisualizacoesAnuncioNaoCompartilhado)
 	{
 		int vezesDozePessoasClicaram = 0;
 		
-		if(qtdVisualizacoesAnuncio >= CEM_VISUALIZACOES)
+		if(qtdVisualizacoesAnuncioNaoCompartilhado >= VISUALIZACOES)
 		{
-			vezesDozePessoasClicaram = qtdVisualizacoesAnuncio / CEM_VISUALIZACOES;
-			return vezesDozePessoasClicaram * DOZE_CLIQUES_ANUNCIO; 
+			vezesDozePessoasClicaram = qtdVisualizacoesAnuncioNaoCompartilhado / VISUALIZACOES;
+			return vezesDozePessoasClicaram * CLIQUES_ANUNCIO; 
 		}
 		else {
 			return 0;
 		}
 	}
 	
-	public static int calculaQuantidadeCompartilhamentoRedeSocial(int qtdCliquesAnuncio)
+	private static int calculaQuantidadeCompartilhamentosNaRedeSocial(int qtdCliquesAnuncio)
 	{
 		int vezesTresPessoasCompartilharam = 0;
 		
-		if(qtdCliquesAnuncio >= VINTE_ACESSOS)
+		if(qtdCliquesAnuncio >= ACESSOS_ANUNCIO)
 		{
-			vezesTresPessoasCompartilharam = qtdCliquesAnuncio / VINTE_ACESSOS;
-			return vezesTresPessoasCompartilharam * TRES_COMPARTILHAMENTOS_ANUNCIO; 
+			vezesTresPessoasCompartilharam = qtdCliquesAnuncio / ACESSOS_ANUNCIO;
+			return vezesTresPessoasCompartilharam * COMPARTILHAMENTOS_ANUNCIO;
 		}
 		else {
 			return 0;
 		}
+	}	
+	
+	/**
+	 * Este método calcula o número máximo de compartilhamentos na rede social considerando a sequência adotada.
+	 * 
+	 * @param totalCompartilhamentos O total de compartilhamentos na rede social desconsiderando a sequência. 
+	 * @return o número máximo de compartilhamentos observada a sequência.
+	 */
+	
+	public static int calculaQuantidadeCompartilhamentosEmSequenciaNaRedeSocial(int totalCompartilhamentos)
+	{
+		
+		int totalCompartilhamentosComSequencia = 0;
+		
+		for(int i = 1; i <= totalCompartilhamentos; i++)
+		{
+			if(i % NUMERO_POS_SEQUENCIA == 0)
+			{
+				continue;
+			}
+			
+			++totalCompartilhamentosComSequencia;
+		}
+		
+		return totalCompartilhamentosComSequencia;
+	}
+	
+	public static int calculaQuantidadeVisualizacoesDoAnuncioCompartilhado(int qtdCompartilhamentoRedeSocial)
+	{
+		return qtdCompartilhamentoRedeSocial * VISUALIZACOES_ANUNCIO_COMPARTILHADO;
 	}
 }
